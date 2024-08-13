aa1ceaa1cb99728fc6ac2cb219596d506ee0f16b
siddharth <sidpuri264@gmail.com>
2024-08-13 12:28:15 +0530

    favouriteBook route done

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,11 @@ require("./connetion/conn");
 
 const userroute = require("./routes/user");
 const bookroute = require("./routes/book");
+const favouriteRoute = require("./routes/favourite");
 
 app.use("/api/v1",userroute);
 app.use("/api/v1",bookroute);
+app.use("/api/v1",favouriteRoute);
 
 app.get("/",(req,res)=>{
     res.send({message:"hello from server"});
